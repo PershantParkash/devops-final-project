56aae0841e486a2817c37231fef1d7d1f9b6a8db
PershantParkash <pershantparkash @gmail.com>
2025-06-18 19:59:51 +0500

    static ip again

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_public_ip" "main" {
   name                = "pip-devops-vm"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  allocation_method = "Dynamic"
+  allocation_method   = "Static"  
 }
 
 # Create a security group (firewall rules)
